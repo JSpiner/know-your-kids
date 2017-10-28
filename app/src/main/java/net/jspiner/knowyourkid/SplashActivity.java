3f374554461b67732c66df38d97ec9ba0e0a9359
jspiner <jspiner@naver.com>
2017-10-28 15:26:45 +0900

    add splash page

--- app/src/main/java/net/jspiner/knowyourkid/SplashActivity.java
+++ app/src/main/java/net/jspiner/knowyourkid/SplashActivity.java
@@ -1,9 +1,33 @@
 package net.jspiner.knowyourkid;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
+        init();
+    }
 
+    private void init() {
+        new Handler().postDelayed(
+                () -> {
+                    startMainActivity();
+                },
+                1000
+        );
+    }
+
+    private void startMainActivity(){
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
